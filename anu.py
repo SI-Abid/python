eb754d80a8ba07d12a7d88065ac456638759de55
saiham <siaabid5323@gmail.com>
2021-11-30 20:26:17 +0600

    anaconda

---
+++ anu.py
@@ -0,0 +1,17 @@
+while True:
+  num1 = int(input('Enter the first number: '))
+  num2 = int(input('Enter the second number: '))
+  ch = input("Enter which operation you want to perform: ")
+  result = 0
+  # switch case
+  switcher = {
+    '+': num1 + num2,
+    '-': num1 - num2,
+    '*': num1 * num2,
+    '/': num1 / num2,
+  }
+  result = switcher.get(ch, "Undefined")
+  print(num1,ch,num2,":",result)
+  d = input('Enter go/exit: ')
+  if d=='exit':
+    break
